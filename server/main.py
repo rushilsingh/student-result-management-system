ca604d99c4395aeae3fc61a5bc00cfd83f26503b
Rushil Singh <rusingh@synamedia.com>
2023-04-25 00:02:19 +0530

    Replace book with student as per requirement

--- server/main.py
+++ server/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from dotenv import dotenv_values
 from pymongo import MongoClient
-from routes import router as book_router
+from routes import router as student_router
 
 config = dotenv_values(".env")
 
@@ -20,4 +20,4 @@ def shutdown_db_client():
     app.mongodb_client.close()
 
 
-app.include_router(book_router, tags=["books"], prefix="/book")
+app.include_router(student_router, tags=["students"], prefix="/student")
--- server/models.py
+++ server/models.py
@@ -3,34 +3,30 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-class Book(BaseModel):
+class Student(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    title: str = Field(...)
-    author: str = Field(...)
-    synopsis: str = Field(...)
+    first_name: str = Field(...)
+    last_name: str = Field(...)
+    email_address: str = Field(...)
 
     class Config:
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
                 "_id": "066de609-b04a-4b30-b46c-32537c7f1f6e",
-                "title": "Don Quixote",
-                "author": "Miguel de Cervantes",
-                "synopsis": "...",
+                "first_name": "Rushil",
+                "last_name": "Singh",
+                "email_address": "rushil@email.com",
             }
         }
 
 
-class BookUpdate(BaseModel):
-    title: Optional[str]
-    author: Optional[str]
-    synopsis: Optional[str]
+class StudentUpdate(BaseModel):
+    email_address: Optional[str]
 
     class Config:
         schema_extra = {
             "example": {
-                "title": "Don Quixote",
-                "author": "Miguel de Cervantes",
-                "synopsis": "Don Quixote is a Spanish novel by Miguel de Cervantes...",
+                "email_address": "rushil2@email.com",
             }
         }
