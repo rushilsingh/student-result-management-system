28d16c60995261e3a3b4451d04aad2bd5833bd4e
Rushil Singh <rusingh@synamedia.com>
2023-04-25 00:39:32 +0530

    Basic pages for Home, CreateStudent, and ListStudents

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import HomePage from "./HomePage";
+import CreateStudentPage from "./CreateStudentPage";
+import StudentListPage from "./StudentListPage";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app-container">
+        <nav className="left-navigation">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/create-student">Create Student</Link>
+            </li>
+            <li>
+              <Link to="/student-list">Student List</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/create-student" element={<CreateStudentPage />} />
+            <Route path="/student-list" element={<StudentListPage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
