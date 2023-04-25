eac29378cffa4fb99d9459555f046fa838658020
Rushil Singh <rusingh@synamedia.com>
2023-04-25 14:16:42 +0530

    Separate student routes

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from dotenv import dotenv_values
 from pymongo import MongoClient
-from routes import router as student_router
+from student_routes import router as student_router
 from fastapi.middleware.cors import CORSMiddleware
 
 config = dotenv_values(".env")
